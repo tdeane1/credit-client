3cf3ea85c9cb108cdb6a3f136a8d965bff52ff79
tdeane1 <trevor.deane@gmail.com>
2020-08-28 10:25:39 -0400

    Home Screen working

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,80 +1,69 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./Home.css";
-import { Form, Row, Container, Button, FormControl } from "react-bootstrap";
-import { useFormFields } from "../libs/hooksLib";
+import { Form, Row, Container, Button, FormControl, ListGroup } from "react-bootstrap";
+
 import { onError } from "../libs/errorLib";
 import { API } from "aws-amplify";
 
 export default function Home() {
-    const [fields, handleFieldChange] = useFormFields({
-        idNumber: "",
-        creditScore: ""
-    });
-
+    const [idNumber, setIdNumber] = useState("");
     const [content, setContent] = useState("");
-    const [showFields, setShowFields] = useState(false);
-
+    
     function validateForm() {
-        console.log(fields.idNumber);
-        console.log(Home.idNumber);
+        console.log(idNumber);           
         return (
-            fields.idNumber.length > 0 &&
-            fields.idNumber.length <12
+            idNumber.length > 0 &&
+            idNumber.length < 12
         );
     }
+
+    function handleChange() {
+        setContent("");
+    }
+
+    function handleIdChange(event) {
+        setIdNumber(event.target.value);
+        setContent("");
+    }
+    
     async function handleSubmit() {
         event.preventDefault();
-
-        /*         console.log(country.value);
-                console.log(idType.value);
-                console.log(fields.idNumber); */
-
         let id = "".concat(country.value.substr(0, 2))
             .concat(idType.value.substr(0, 2))
-            .concat("0".repeat(11 - fields.idNumber.length)
-                .concat(fields.idNumber));
+            .concat("0".repeat(11 - idNumber.length)
+                .concat(idNumber));
         console.log(id);
-        setShowFields(false)
-
-
         try {
             await API.get("creditScore", `/creditScore/${id}`)
-                .then(content => {
-                    console.log(content);
-                   /*  outPutFields.creditScore = content.creditScore;
-                    outputFields.creditScoreRemarks = content.creditScoreRemarks;
-                    outputFields.lastName = content.lastName; */
-                    setContent(content);
-                    console.log(content.creditScore);
-                    creditScoreField.disabled="false";
-                    setShowFields(true)
-                    
-
+                .then(contentReturned => {
+                    console.log(contentReturned);
+                    /*  outPutFields.creditScore = content.creditScore;
+                     outputFields.creditScoreRemarks = content.creditScoreRemarks;
+                     outputFields.lastName = content.lastName; */
+                    setContent(contentReturned);
+                    //console.log("this is line 47 " + content.creditScore);
+                    //creditScoreField.disabled = "false";
                 }
-                
-            )
+                );
         }
         catch (e) {
-            setShowFields(false);
-            onError(e);
-            
+            setContent("");
+            //onError(e);
         }
-
-
     }
+
     function renderForm() {
         return (
-
             <div >
                 <h1>Credit Note Check</h1>
                 <Form onSubmit={handleSubmit}>
-
                     <Form.Group controlId="country">
                         <Form.Label>Select Country ID is from</Form.Label>
                         <Form.Control as="select"
                             size="sm"
                             autoFocus
-                            onSubmit={handleFieldChange}>
+                            onChange={handleChange}
+                        >
                             <option>tt-Trinidad and Tobago</option>
                             <option>bd-Barbados</option>
                             <option>us-United States</option>
@@ -84,7 +73,8 @@ export default function Home() {
                         <Form.Label>Select Type of Id</Form.Label>
                         <Form.Control as="select"
                             size="sm"
-                            onSubmit={handleFieldChange}>
+                            onChange={handleChange}
+                        >
                             <option>dp-Drivers Permit</option>
                             <option>pp-Passport</option>
                             <option>id-National ID Card</option>
@@ -96,8 +86,8 @@ export default function Home() {
                             size="sm"
                             type="input"
                             placeholder="Enter ID Number"
-                            value={fields.password}
-                            onChange={handleFieldChange}>
+                            value={idNumber}
+                            onChange={handleIdChange}>
                         </Form.Control>
                     </Form.Group>
                     <Form.Group>
@@ -107,61 +97,18 @@ export default function Home() {
                             disabled={!validateForm()}
                         >Get Credit Score
                     </Button>
-
-                    </Form.Group>
-                    <Form.Group controlId="creditScoreField">
-                        <Form.Label>Credit Score</Form.Label>
-                        <Form.Control
-                            disabled="false"
-                            value={content.creditScore}
-                            onChange={e => setContent(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group><Form.Group controlId="creditScoreRemarks">
-                        <Form.Label>Credit Remarks</Form.Label>
-                        <Form.Control
-                            disabled="false"
-                            value={content.creditScoreRemarks}
-                            onChange={e => setContent(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="lastName">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control
-                            disabled="false"
-                            value={content.lastName}
-                            onChange={e => setContent(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
-
-
+                   </Form.Group>
+                    <ListGroup >
+                        <ListGroup.Item
+                        >Credit Score   : {content.creditScore} </ListGroup.Item>
+                        <ListGroup.Item>Credit Remarks : {content.creditScoreRemarks} </ListGroup.Item>
+                        <ListGroup.Item>Last Name      : {content.lastName} </ListGroup.Item>
+                    </ListGroup>
                 </Form>
-
             </div >
-
         );
     }
 
-    function RenderCreditScore() {
-        console.log(content.creditScore);
-        return (
-            <div>
-                <Form >
-
-                    <Form.Group controlId="score">
-                        <Form.Label>Credit Score</Form.Label>
-                        <Form.Control as="readOnly"
-                            size="sm"
-
-                            value={content.creditScore}>
-
-                        </Form.Control>
-                    </Form.Group>
-                </Form>
-
-            </div>
-
-        );
-    }
     return (
         <div className="Home">
             {renderForm()}
